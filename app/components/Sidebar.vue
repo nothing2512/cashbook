e76a323dc76275436e727464f6d3294a0d2ea461
samtebor <robet.personal@gmail.com>
2026-05-02 23:22:22 +0700

    theming

--- app/components/Sidebar.vue
+++ app/components/Sidebar.vue
@@ -34,6 +34,15 @@ const getData = async () => {
     document.documentElement.setAttribute('data-bs-theme', setting.value.theme)
 }
 
+const changeTheme = async () => {
+    setting.value.theme = setting.value.theme == 'dark' ? 'light' : 'dark'
+    document.documentElement.setAttribute('data-bs-theme', setting.value.theme)
+    await fetchSetting.edit({
+        id: 1,
+        theme: setting.value.theme
+    })
+}
+
 const onSubmitModal = async (form) => {
     await fetchSetting.edit(form)
     await getData()
@@ -75,7 +84,7 @@ const onCloseModal = async () => {
                         </svg>
                         <div class="form-check form-switch fs-6">
                             <input class="form-check-input  me-0" type="checkbox" id="toggle-dark"
-                                style="cursor: pointer">
+                                style="cursor: pointer" @click="changeTheme" :checked="setting?.theme == 'dark'">
                             <label class="form-check-label"></label>
                         </div>
                         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
