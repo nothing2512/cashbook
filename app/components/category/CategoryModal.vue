265fab22db95ca4cdeaae3ed67c133641bb90554
samtebor <samtebor@samtebors-MacBook-Pro.local>
2026-05-10 21:13:09 +0700

    fix button

--- app/components/category/CategoryModal.vue
+++ app/components/category/CategoryModal.vue
@@ -73,12 +73,10 @@ const onSubmit = () => {
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
-                                <i class="bx bx-x d-block d-sm-none"></i>
-                                <span class="d-none d-sm-block">Close</span>
+                                <span class="d-sm-block">Close</span>
                             </button>
                             <button type="button" class="btn btn-primary ms-1" @click.prevent="onSubmit">
-                                <i class="bx bx-check d-block d-sm-none"></i>
-                                <span class="d-none d-sm-block">Submit</span>
+                                <span class="d-sm-block">Submit</span>
                             </button>
                         </div>
                     </form>
